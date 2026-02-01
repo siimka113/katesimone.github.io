675964bf1c5d255fc2891615fbf4e614b24b1564
siimka113 <kate.simone113@gmail.com>
2026-02-01 01:54:39 -0800

    Home Page

---
+++ main.js
@@ -0,0 +1,34 @@
+let wordIndex = 0;
+const words = ["PEOPLE", "TECHNICIANS", "DOCTORS", "EDUCATORS", "VOLUNTEERS", "OPERATORS"];
+
+function changeText() {
+  const paragraphElement = document.getElementById("rotating-text");
+
+  paragraphElement.classList.remove('animate-swap');
+  void paragraphElement.offsetWidth; 
+  paragraphElement.classList.add('animate-swap');
+
+  setTimeout(() => {
+    wordIndex = (wordIndex + 1) % words.length; 
+    paragraphElement.textContent = words[wordIndex];
+  }, 250);
+}
+
+setInterval(changeText, 1200); 
+
+function updateTime() {
+  const timeElement = document.querySelector('.time');
+  const options = { 
+    timeZone: 'America/Vancouver', 
+    hour: '2-digit', 
+    minute: '2-digit', 
+    hour12: false 
+  };
+  const formatter = new Intl.DateTimeFormat('en-CA', options); 
+  const currentTime = formatter.format(new Date());
+  
+  timeElement.textContent = currentTime;
+}
+updateTime(); 
+setInterval(updateTime, 1000); // 1000ms is better for real-time updates
+
